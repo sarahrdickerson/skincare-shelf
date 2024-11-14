9d3f794bc56cc6c20c11c87a8db034d1b572ca66
Sarah Dickerson <sarah.dickerson@aa.com>
2024-11-14 17:38:32 -0600

    refactor: add google logo

--- client/app/(auth)/(signup)/signup-options/page.tsx
+++ client/app/(auth)/(signup)/signup-options/page.tsx
@@ -3,12 +3,13 @@ import SignupForm from '@/components/auth/SignupForm'
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { GoogleLogo } from '@phosphor-icons/react';
+import Image from 'next/image';
 import React from 'react'
 
 const SignupOptionsPage = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 min-h-screen'>
-        <div className="flex flex-col items-center p-4 space-y-4 px-8 pt-12">
+        <div className="flex flex-col items-center p-4 space-y-4 lg:px-8 pt-12 px-16">
             {/* Content for the first column */}
             <div className='flex flex-col space-y-2 text-center'>
                 <h2 className="text-2xl font-bold px-4">Ready to Transform Your Skincare Routine?</h2>
@@ -16,9 +17,12 @@ const SignupOptionsPage = () => {
             </div>
             <div className='flex flex-col space-y-6 pt-4 flex-grow w-full lg:max-w-lg'>
                 <Button variant='outline'
-                    className='flex flex-row'
+                    className='flex flex-row items-center justify-center'
                 >
-                    <GoogleLogo/>Continue with Google
+                    <div className=''>
+                        <Image src='/google-icon.svg' alt='Google Logo' width={16} height={16} className=''/>
+                    </div>
+                    <p className=''>Sign up with Google</p>
                 </Button>
                 <div className='flex items-center justify-center space-x-4'>
                     <Separator className='flex-1' />
