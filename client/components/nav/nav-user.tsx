22bcf0fcf9c30aeff726a564c351009b32903931
Sarah Dickerson <sarah.dickerson@aa.com>
2024-11-18 10:20:38 -0600

    refactor: functional user nav links

--- client/components/nav/nav-user.tsx
+++ client/components/nav/nav-user.tsx
@@ -6,7 +6,9 @@ import {
   ChevronsUpDown,
   CreditCard,
   LogOut,
+  Settings,
   Sparkles,
+  User,
 } from "lucide-react"
 
 import {
@@ -33,6 +35,7 @@ import { useRouter } from "next/navigation"
 import { useUser } from "@/context/UserContext"
 import UserAvatar from "../user/user-avatar"
 import UserCard from "../user/user-card"
+import Link from "next/link"
 
 export function NavUser() {
   const { isMobile } = useSidebar();
@@ -80,16 +83,22 @@ export function NavUser() {
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <BadgeCheck />
-                Account
+                  <Link href="/profile" className="flex flex-row space-x-2 items-center">
+                    <User className="h-4 w-4"/>
+                    <p>Profile</p>
+                  </Link>
               </DropdownMenuItem>
               <DropdownMenuItem>
-                <CreditCard />
-                Billing
+                  <Link href="/profile" className="flex flex-row space-x-2 items-center">
+                        <Settings className="h-4 w-4"/>
+                        <p>Settings</p>
+                  </Link>
               </DropdownMenuItem>
               <DropdownMenuItem>
-                <Bell />
-                Notifications
+                  <Link href="/profile" className="flex flex-row space-x-2 items-center">
+                    <Bell className="h-4 w-4"/>
+                    <p>Notifications</p>
+                  </Link>
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
